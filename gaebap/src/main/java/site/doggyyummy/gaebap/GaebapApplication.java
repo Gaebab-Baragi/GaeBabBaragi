de8bee6f58b7dadb628619ef43c91b84e4d619d5
박영서 <pj0642@gmail.com>
2023-07-17 16:47:42 +0900

    initial commit

---
+++ gaebap/src/main/java/site/doggyyummy/gaebap/GaebapApplication.java
@@ -0,0 +1,13 @@
+package site.doggyyummy.gaebap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaebapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaebapApplication.class, args);
+	}
+
+}
---
+++ gaebap/src/test/java/site/doggyyummy/gaebap/GaebapApplicationTests.java
@@ -0,0 +1,13 @@
+package site.doggyyummy.gaebap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaebapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
