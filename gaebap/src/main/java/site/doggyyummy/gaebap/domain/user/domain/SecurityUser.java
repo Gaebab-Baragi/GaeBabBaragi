8f108b0acf521aa4f019ca979e9b07b633ec767e
박영서 <pj0642@gmail.com>
2023-07-19 12:59:21 +0900

    FEAT) SecurityUser entity를 추가 - SecurityUser는 User 클래스 객체를 Wrapping해서 사용 - 이후 권한 확인 등을 처리하기 위한 객체로  사용 예정

---
+++ gaebap/src/main/java/site/doggyyummy/gaebap/domain/user/domain/SecurityUser.java
@@ -0,0 +1,50 @@
+package site.doggyyummy.gaebap.domain.user.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+    private final User user;
+
+    public SecurityUser(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(()-> this.user.getAuthority()); //추후에 수정
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
