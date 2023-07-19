361c5a320871b4e81b80e20e42e0efba5e97aefc
박영서 <pj0642@gmail.com>
2023-07-19 17:48:23 +0900

    FEAT) MemberContoller 클래스 추가 - 테스트를 위한 hello 출력

--- gaebap/src/main/java/site/doggyyummy/gaebap/global/security/config/SecurityConfig.java
+++ gaebap/src/main/java/site/doggyyummy/gaebap/global/security/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package site.doggyyummy.gaebap.global.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -34,4 +38,17 @@ public class SecurityConfig {
         return userDetailsService;
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //TODO
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests.anyRequest().permitAll()
+                );
+        return http.build();
+    }
 }
