5ebf4d4f2bdd1dbb9d9fc17e2cb6ebe5d54e3f96
Park Yeongseo <pj0642@gmail.com>
2023-07-19 22:48:37 +0900

    FEAT) MemberDetailsDTO, MemberService 클래스 생성 - MemberDetailDTO는 회원가입 및 회원 정보 수정에서 사용할 수 있음

---
+++ gaebap/src/main/java/site/doggyyummy/gaebap/domain/member/dto/MemberDetailsDTO.java
@@ -0,0 +1,11 @@
+package site.doggyyummy.gaebap.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class MemberDetailsDTO {
+    private String name;
+    private String password;
+    private String nickname;
+    private String email;
+}
---
+++ gaebap/src/main/java/site/doggyyummy/gaebap/domain/member/sevice/MemberService.java
@@ -0,0 +1,6 @@
+package site.doggyyummy.gaebap.domain.member.sevice;
+
+
+public class MemberService {
+
+}
