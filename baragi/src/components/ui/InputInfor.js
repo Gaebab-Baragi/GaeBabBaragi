deef85a0bb83e9a272703a3eeba49a558a80e4da
June Hyoung <zxc4554@naver.com>
2023-07-24 17:50:56 +0900

    다시수정

--- baragi/src/components/ui/InputInfor.js
+++ baragi/src/components/ui/InputInfor.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 function InputInfor() {
   const [recipeName, setRecipeName] = useState('');
   const [foodName, setFoodName] = useState('');
-  const [recipeIntro, setRecipeIntro] = useState('')
+  const [recipeIntro, setRecipeIntro] = useState('');
+
   const handleRecipeNameChange = (e) => {
     setRecipeName(e.target.value);
   };
@@ -21,70 +22,46 @@ function InputInfor() {
     // 레시피 이름과 음식 이름을 서버로 전송하거나 다른 작업을 수행하는 로직을 추가할 수 있습니다.
     console.log('레시피 이름:', recipeName);
     console.log('음식 이름:', foodName);
+    console.log('레시피 소개:', recipeIntro);
   };
 
   return (
-<<<<<<< HEAD
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="recipeName">레시피 이름:</label>
-      <input
-        type="text"
-        id="recipeName"
-        name="recipeName"
-        value={recipeName}
-        onChange={handleRecipeNameChange}
-        placeholder="레시피 이름을 입력하세요."
-      />
-=======
     <>
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'left', padding: '10px 0'}}>
-      <h4>2. 기본 정보 입력</h4>
-    </div>
->>>>>>> 0bad353c92aa678bc4b2e4ad26003689b1149a9b
-
       <form onSubmit={handleSubmit}>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <label htmlFor="recipeName" style={{ marginRight: '10px' }}>레시피 제목:</label>
-        <input
-          type="text"
-          id="recipeName"
-          name="recipeName"
-          value={recipeName}
-          onChange={handleRecipeNameChange}
-          placeholder="레시피 제목을 입력하세요."
-        />
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <label htmlFor="recipeName" style={{ marginRight: '10px' }}>레시피 제목:</label>
+          <input
+            type="text"
+            id="recipeName"
+            name="recipeName"
+            value={recipeName}
+            onChange={handleRecipeNameChange}
+            placeholder="레시피 제목을 입력하세요."
+          />
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <label htmlFor="foodName" style={{ marginRight: '10px' }}>음식 이름:</label>
-        <input
-          type="text"
-          id="foodName"
-          name="foodName"
-          value={foodName}
-          onChange={handleFoodNameChange}
-          placeholder="음식 이름을 입력하세요."
-        />
+          <label htmlFor="foodName" style={{ marginRight: '10px' }}>음식 이름:</label>
+          <input
+            type="text"
+            id="foodName"
+            name="foodName"
+            value={foodName}
+            onChange={handleFoodNameChange}
+            placeholder="음식 이름을 입력하세요."
+          />
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <label htmlFor="recipeIntro" style={{ marginRight: '10px' }}>레시피 소개:</label>
-        <input
-          type="text"
-          id="recipeIntro"
-          name="recipeIntro"
-          value={recipeIntro}
-          onChange={handleRecipeIntroChange}
-          placeholder="레시피를 소개하세요."
-        />
+          <label htmlFor="recipeIntro" style={{ marginRight: '10px' }}>레시피 소개:</label>
+          <input
+            type="text"
+            id="recipeIntro"
+            name="recipeIntro"
+            value={recipeIntro}
+            onChange={handleRecipeIntroChange}
+            placeholder="레시피를 소개하세요."
+          />
         </div>
-
-
-
-
-
-
-        
-
-        {/* <button type="submit">제출</button> */}
+        <button type="submit">제출</button>
       </form>
     </>
   );
