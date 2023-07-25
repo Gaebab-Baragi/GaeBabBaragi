160478ac8d307a15bdd22769f3dee9b35b14c104
hanulkimm <hanulkim2000@naver.com>
2023-07-26 00:15:35 +0900

    REFACTOR) 로그인, 회원가입 기능 구현 ADD) redux userslice 생성

--- baragi/src/components/form/LoginForm.js
+++ baragi/src/components/form/LoginForm.js
@@ -1,16 +1,72 @@
-import React from 'react';
-import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { redirect, useNavigate} from 'react-router-dom'
 import { useState } from 'react';
 import './BasicForm.css'
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../redux/userSlice';
+
 
 function LoginForm() {
   const navigate = useNavigate();
-  let [id, setId] = useState();
-  let [password, setPassword] = useState();
+  const dispatch = useDispatch();
+  let [id, setId] = useState('');
+  let [password, setPassword] = useState('');
+  let [msg, setMsg] = useState('')
+
+  // 오류 메세지 alert
+  useEffect(()=>{
+    if (msg) {
+      alert(msg)
+    }
+  },[msg])
+
+
+  // ----------- 로그인 기능 구현-------------------//
+  const handleLogin = (e) => {
+    // Reload 막기
+    e.preventDefault();
+    // 빈 값 처리
+    if (!id) {
+      return alert('아이디를 입력하세요.')
+    } else if (!password) {
+      return alert('비밀번호를 입력하세요')
+    }
+    // axios 요청 보내기
+    let body = {
+      id,
+      password
+    };
+
+    axios.post('/member/login', body)
+    .then((res)=>{
+      console.log(res.data);
+      switch (res.data.code) {
+        case 200:
+          console.log("로그인");
+          dispatch(loginUser(res.data.userInfo));
+          break;
+        case 400:
+          setMsg("아이디, 비밀번호가 비어있습니다.");
+          break;
+        case 401:
+          setMsg("존재하지 않는 아이디입니다.");
+          break;
+        case 402:
+          setMsg("비밀번호가 틀렸습니다.");
+          break;
+        default:
+          break;
+      }
+    })
+
+  }
+  // ---------------------------------------------//
+
 
   return (
     <div className="formContainer">
-      <form className="form">
+      <form onSubmit={handleLogin} className="form">
         <div className="formTitle">로그인</div>
 
         {/* 아이디 입력  */}
@@ -46,6 +102,3 @@ function LoginForm() {
 }
 
 export default LoginForm;
-
-// 아이디, 비밀번호로 로그인 function 만들기
-// 어떻게? back-end랑 얘기해보기
--- baragi/src/components/form/SignupForm.js
+++ baragi/src/components/form/SignupForm.js
@@ -2,58 +2,106 @@ import React from 'react';
 import './BasicForm.css';
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
 import { useState } from 'react';
+import axios from 'axios';
 
 function SignupForm() {
   const navigate = useNavigate();
-  let [id, setId] = useState();
-  let [password1, setPassword1] = useState();
-  let [password2, setPassword2] = useState();
-  let [nickname, setNickname] = useState();
-  let [email, setEmail] = useState();
+  let [id, setId] = useState('');
+  let [password1, setPassword1] = useState('');
+  let [password2, setPassword2] = useState('');
+  let [nickname, setNickname] = useState('');
+  let [email, setEmail] = useState('');
+  let [code, setCode] = useState();
+  let [sendCode, setSendCode] = useState(false);
 
   // 아이디 중복 체크 function
-  const handleDuplicateCheck = ()=>{
+  const handleIdDuplicateCheck = ()=>{
     alert('중복확인필요');
   }
+  // 닉네임 중복 체크 function
+  const handleNicknameDuplicateCheck = ()=>{
+    alert('닉네임 확인')
+  }
+
+  // 인증번호 입력창 보여주기 위해 state 변경
+  const handleSendCode = () => {
+    setSendCode(true);
+  }
+
+  //--------------- 회원 가입 기능 구현-------------------//
+  // 다른 버튼 누를 때 axois 요청되는 문제 해결하기
+  // 빈 값 있을 때 오류 msg 보여주기
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(password1);
+    console.log(id);
+    console.log(nickname);
+    console.log(email);
+
+    let body = {
+      registerName : id,
+      password : password1,
+      nickname : nickname,
+      email : email
+    };
+    // axios.post('/member/register', body)
+    // .then((res)=>{
+    //   if (res.data.code ===200) {
+    //     console.log('signup success')
+    //     navigate('/login')
+    //   }
+    // })
+  }
+  //----------------------------------------------------------//
+
 
   return (
     <div className="formContainer">
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <div className="formTitle">회원가입</div>
 
         {/* 아이디 입력  */}
         <div className="formGroup" id='checkGroup'>
           <input className='formInput' onChange={e=>{setId(e.target.value)}} type="id" id="formId" placeholder="아이디를 입력해주세요." />
-          <button className='duplicationCheckButton' onClick={handleDuplicateCheck}>중복 확인</button>
+          <button className='duplicationCheckButton' onClick={()=>handleIdDuplicateCheck()}>중복 확인</button>
         </div>
 
         {/* 비밀번호1 입력 */}
         <div className="formGroup">
-          <input className='formInput' onChange={e=>{setPassword1(e.target.value)}} type="password" id="formPassword" placeholder="비밀번호를 입력해주세요." />
+          <input className='formInput' onChange={e=>{setPassword1(e.target.value)}} type="password" id="formPassword1" placeholder="비밀번호를 입력해주세요." />
         </div>
         {/* 비밀번호2 다시 입력 */}
         <div className="formGroup">
-          <input className='formInput' onChange={e=>{setPassword2(e.target.value)}} type="password" id="formPassword" placeholder="비밀번호를 다시 입력해주세요." />
+          <input className='formInput' onChange={e=>{setPassword2(e.target.value)}} type="password" id="formPassword2" placeholder="비밀번호를 다시 입력해주세요." />
         </div>
 
         {/* 닉네임 입력 */}
         <div className="formGroup">
           <input className='formInput' onChange={e=>{setNickname(e.target.value)}} type="text" id='formNickname' placeholder='닉네임을 입력해주세요'/>
+          <button className='duplicationCheckButton' onClick={()=>handleNicknameDuplicateCheck()}>중복 확인</button>
         </div>
 
-        {/* 이메일 입력 */}
+        {/* 이메일 입력 & 인증번호 요청하기 */}
         <div className="formGroup" id='checkGroup'>
           <input className='formInput' onChange={e=>{setEmail(e.target.value)}} type="email" id='formEmail' placeholder='이메일을 입력해주세요.' />
-          <button className='emailVerificationButton'>인증코드 발송</button>
+          <button onClick={()=>handleSendCode()} className='emailVerificationButton'>인증코드 발송</button>
         </div>
 
+        {/* 인증코드 입력 */}
+        { sendCode && (
+        <div className="formGroup">
+          <input className='formInput' onChange={e=>{setCode(e.target.value)}} type="text"  placeholder="인증번호 입력" />
+          <button className='verificationBtn'>확인</button>
+        </div>
+        )
+        }
 
         {/* 로그인 navigate */}
         <div className='formGroup'>
           이미 회원이신가요?<span className='loginRoute' onClick={()=>{navigate('/login')}}>로그인</span> 
         </div>
 
-        {/* Login 버튼 */}
+        {/* Signup 버튼 */}
         <button className="submitButton" type="submit">
           SIGNUP
         </button>
