c9e9fc7847c8b4061922671e7c636b55cb7fe379
Park Yeongseo <pj0642@gmail.com>
2023-07-26 23:20:17 +0900

    FEAT) 회원가입 axios 작성 - 아이디 정규표현식 변경(영어소문자로 시작. 숫자로만 작성하는 것은 불가능. 소문자로만 작성하는 것은 가능) - 이메일 발송 및 확인 가능 - 회원 가입 후 로그인 페이지로 이동 - 근데 각 값들이 제대로 갱신되지 않는 경우가 있음. 확인 필요

--- baragi/src/components/form/SignupForm.js
+++ baragi/src/components/form/SignupForm.js
@@ -12,6 +12,7 @@ function SignupForm() {
   let [password2, setPassword2] = useState('');
   let [nickname, setNickname] = useState('');
   let [email, setEmail] = useState('');
+  let [verificationCode, setVerificationCode] = useState('')
   let [code, setCode] = useState();
   let [sendCode, setSendCode] = useState(false);
 
@@ -23,7 +24,7 @@ function SignupForm() {
   let [validEmail, setValidEmail] = useState(false); 
   useEffect(() => {
     // 정규표현식 패턴: 5자 이상 20자 이하, 영어 소문자, 숫자, 특수문자를 하나 이상 포함
-    const idPattern = /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-z0-9!@#$%^&*]{5,20}$/i;
+    const idPattern = /^[a-z]{1}[a-z0-9_]{4,19}$/; 
     const isValidId = idPattern.test(id);
     setValidId(isValidId);
   }, [id]);
@@ -50,25 +51,99 @@ function SignupForm() {
   const handleIdDuplicateCheck = useCallback((e) => {
     e.preventDefault();
     console.log('idDuplicationCheck')
+    console.log(id)
     // 
-    setIdDuplicateCheck(true);
+    let body = JSON.stringify({
+      register_name : id,
+      password : password1,
+      nickname : nickname,
+      email : email
+    })
+
+    axios.post('/member/register/id', body, {
+      headers: { "Content-Type": `application/json; charset=UTF-8`}
+      })
+    .then((res)=>{
+      if (res.status === 200) {
+        console.log('duplication checked')
+        setIdDuplicateCheck(true);
+      }
+    })
+    .catch((res) => {
+      if (res.status === 459) {
+        alert("잘못된 아이디 형식입니다.")
+      }
+      else if (res.status === 456){
+        alert("이미 사용중인 아이디입니다.")
+      }
+    })
   }, []);
 
   const handleNicknameDuplicateCheck = useCallback((e) => {
     e.preventDefault();
     console.log('Nickname-Duplication-Check')
-    setNicknameDuplicateCheck(true);
+    console.log(nickname)
+    let body = JSON.stringify({
+      register_name : id,
+      password : password1,
+      nickname : nickname,
+      email : email
+    })
+
+    axios.post('/member/register/nickname', body, {
+      headers: { "Content-Type": `application/json; charset= UTF-8`}
+      })
+    .then((res)=>{
+      if (res.status === 200) {
+        console.log('duplication checked')
+        setNicknameDuplicateCheck(true);
+      }
+    })
+    .catch((res) => {
+      if (res.status === 460) alert("잘못된 닉네임 형식입니다.")
+      else if (res.status === 457) alert("이미 사용중인 닉네임입니다.")
+    })
+
   }, []);
 
   const handleEmailCodeCheck = useCallback((e) => {
     e.preventDefault();
     console.log('Email-Code-Check')
-    setEmailCodeCheck(true);
+    if (code === verificationCode) {
+      setEmailCodeCheck(true)
+      alert("확인되었습니다.")
+    }
+    else {
+      setEmailCodeCheck(false)
+      alert("인증번호가 맞지 않습니다.")
+    }
   }, []);
 
   const handleSendCode = useCallback((e) => {
     e.preventDefault()
-    setSendCode(true);
+    let body = JSON.stringify({
+      register_name : id,
+      password : password1,
+      nickname : nickname,
+      email : email
+    })
+
+    axios.post('/member/register/email', body, {
+      headers: { "Content-Type": `application/json; charset= UTF-8`}
+      })
+    .then((res)=>{
+      if (res.status === 200) {
+        console.log('code sent')
+        alert("인증 코드가 발송되었습니다. 이메일을 확인해주세요.")
+        setVerificationCode(res.data);
+        setEmailCodeCheck(false);
+        setSendCode(true);
+      }
+    })
+    .catch((res) => {
+      if (res.status === 454) alert("잘못된 이메일입니다.")
+      else if (res.status === 455) alert("이미 사용중인 이메일입니다.")
+    })
   }, []);
 
   //==================================================================================//
@@ -94,11 +169,14 @@ function SignupForm() {
         };
         axios.post('/member/register', body)
         .then((res)=>{
-          if (res.data.code ===200) {
+          if (res.status ===201) {
             console.log('signup success')
             navigate('/login')
           }
         })
+        .catch(
+          alert("잘못된 회원가입입니다.")
+        );
       }
     }
   //----------------------------------------------------------//
@@ -112,11 +190,11 @@ function SignupForm() {
         {/* 아이디 입력  */}
         <div className="formGroup" id='checkGroup'>
           <div className='formGroupComponent'>
-            <input className='formInput' onChange={e=>{setId(e.target.value)}} type="id" id="formId" placeholder="아이디를 입력해주세요." />
+            <input className='formInput' onChange={e=>{setId(e.target.value); setIdDuplicateCheck(false)}} type="id" id="formId" placeholder="아이디를 입력해주세요." />
             <button className='duplicationCheckButton' onClick={handleIdDuplicateCheck}>중복 확인</button>
           </div>
           { !validId && id.length>=1 && (
-            <div className='errorMsg'>아이디는 영어 소문자, 숫자가 하나 이상 포함되게 5자 이상으로 작성해주세요. </div>
+            <div className='errorMsg'>아이디는 영어 소문자나 숫자가 하나 이상 포함되게 5자 이상으로 작성해주세요. </div>
           )}
         </div>
 
@@ -126,7 +204,7 @@ function SignupForm() {
             <input className='formInput' onChange={e=>{setPassword1(e.target.value)}} type="password" id="formPassword1" placeholder="비밀번호를 입력해주세요." />
           </div>
           { !validPassword && password1.length>=1 && (
-            <div className='errorMsg'>비밀번호는 특수문자, 숫자, 영문자를 하나 이상 포함되게 8자 이상으로 작성해주세요. </div>
+            <div className='errorMsg'>비밀번호는 특수문자, 숫자, 영문자가 각각 하나 이상 포함되게 8자 이상으로 작성해주세요. </div>
           )}
         </div>
         {/* 비밀번호2 다시 입력 */}
@@ -142,7 +220,7 @@ function SignupForm() {
         {/* 닉네임 입력 */}
         <div className="formGroup">
           <div className="formGroupComponent">
-            <input className='formInput' onChange={e=>{setNickname(e.target.value)}} type="text" id='formNickname' placeholder='닉네임을 입력해주세요'/>
+            <input className='formInput' onChange={e=>{setNickname(e.target.value); setNicknameDuplicateCheck(false)}} type="text" id='formNickname' placeholder='닉네임을 입력해주세요'/>
             <button className='duplicationCheckButton' onClick={handleNicknameDuplicateCheck}>중복 확인</button>
           </div>
           { !validNickname && nickname.length>=1 && (
@@ -153,7 +231,7 @@ function SignupForm() {
         {/* 이메일 입력 & 인증번호 요청하기 */}
         <div className="formGroup" id='checkGroup'>
           <div className="formGroupComponent">
-            <input className='formInput' onChange={e=>{setEmail(e.target.value)}} type="email" id='formEmail' placeholder='이메일을 입력해주세요.' />
+            <input className='formInput' onChange={e=>{setEmail(e.target.value); setSendCode(false)}} type="email" id='formEmail' placeholder='이메일을 입력해주세요.' />
             <button onClick={handleSendCode} className='emailVerificationButton'>인증코드 발송</button>
           </div>
         </div>
