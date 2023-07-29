7a07201a476d1ce621de0b6e62990941694b6625
Park Yeongseo <pj0642@gmail.com>
2023-07-29 23:39:30 +0900

    FEAT) 로그인 성공시 redirect하게 만듦 - 이후 redirect된 페이지에서 새로 request를 보내 회원 정보를 받아갈 수 있게 수정

--- gaebap/src/main/java/site/doggyyummy/gaebap/global/security/controller/Oauth2Controller.java
+++ gaebap/src/main/java/site/doggyyummy/gaebap/global/security/controller/Oauth2Controller.java
@@ -1,17 +1,34 @@
 package site.doggyyummy.gaebap.global.security.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import site.doggyyummy.gaebap.domain.member.dto.response.MemberResponseDTO;
+import site.doggyyummy.gaebap.domain.member.entity.Member;
+import site.doggyyummy.gaebap.domain.member.exception.custom.NoSuchUserException;
+import site.doggyyummy.gaebap.domain.member.service.MemberService;
+import site.doggyyummy.gaebap.global.security.service.JwtService;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class Oauth2Controller {
 
+    private final JwtService jwtService;
+    private final MemberService memberService;
+
     @GetMapping("/oauth2/success")
-    public ResponseEntity<MemberResponseDTO> getMemberDTOByAccessToken(@Param(""))
+    public ResponseEntity<MemberResponseDTO> getMemberDTOByAccessToken(String token) throws Exception{
+        log.info("accessToken : {}", token);
+        String name = jwtService.extractName(token).orElseThrow(() -> new Exception());
+        Member member = memberService.findByName(name).orElseThrow(() -> new NoSuchUserException());
 
-
+        log.info("send member_info : {}", member.toString());
+        return new ResponseEntity<>(MemberResponseDTO.toDTO(member), HttpStatus.OK);
+    }
 }
--- gaebap/src/main/java/site/doggyyummy/gaebap/global/security/handler/oauth2/OAuth2LoginSuccessHandler.java
+++ gaebap/src/main/java/site/doggyyummy/gaebap/global/security/handler/oauth2/OAuth2LoginSuccessHandler.java
@@ -7,8 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 import site.doggyyummy.gaebap.domain.member.dto.response.MemberResponseDTO;
 import site.doggyyummy.gaebap.domain.member.entity.Member;
 import site.doggyyummy.gaebap.domain.member.repository.MemberRepository;
@@ -25,19 +28,23 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtService jwtService;
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final MemberRepository memberRepository;
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         try {
             log.info("onAuthenticationSuccess");
             CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-            loginSuccess(response, oAuth2User);
+            loginSuccess(request, response, oAuth2User);
         } catch (Exception e){
            throw e;
         }
     }
 
-    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException{
+    private void loginSuccess(HttpServletRequest request, HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException{
+        if (response.isCommitted()) return;
+
+        //새 accessToken & refreshToken을 만들어서 배정함
         String accessToken = jwtService.createAccessToken(oAuth2User.getName());
         String refreshToken = jwtService.createRefreshToken();
 
@@ -52,5 +59,12 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         MemberResponseDTO responseDTO = MemberResponseDTO.toDTO(member);
         String result = objectMapper.writeValueAsString(responseDTO);
         response.getWriter().println(result);
+        redirectStrategy.sendRedirect(request, response, makeRedirectUrl(accessToken) );
+        log.info("token : {}", accessToken);
+    }
+
+    private String makeRedirectUrl(String accessToken){
+       return UriComponentsBuilder.fromUriString("http://localhost:3000/oauth2/redirect/" + accessToken)
+               .build().toUriString();
     }
 }
