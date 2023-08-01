53bdc9879795c6450edf1c0acd8e8a43417c0aa1
June Hyoung <zxc4554@naver.com>
2023-08-01 16:16:06 +0900

    console

--- baragi/src/components/ui/InputCookstep.js
+++ baragi/src/components/ui/InputCookstep.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 
-function InputCookstep({step}) {
-    const [Cookstep, setCookstep] = useState('');
-    const handleCookstepChange = (e) => {
-      setCookstep(e.target.value);
-      };
+function InputCookstep({ step }) {
+  const [cookstepValue, setCookstepValue] = useState('');
+
+  const handleCookstepChange = (e) => {
+    setCookstepValue(e.target.value);
+    console.log(step, cookstepValue); // 이렇게 사용하세요. 이제 변수 이름이 변경되었습니다.
+  };
+
   return (
     <>
-    <div style={{ display: 'flex', justifyContent: 'space-between', width : '200%' , alignItems: 'center'}}>
-      <label htmlFor="Cookstep" style={{ }}>{`STEP${step}:`}</label>
-      <textarea
-        rows="5" // 여러 줄 입력 필드의 높이를 5줄로 설정  
-        cols="30" // 여러 줄 입력 필드의 너비를 30글자로 설정
-
-        id="Cookstep"
-        name="Cookstep"
-        value={Cookstep}
-        onChange={handleCookstepChange}
-        placeholder="예)요리방법 돼지고기 소고기 요리해줘"
-      />
-    </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '200%', alignItems: 'center' }}>
+        <label htmlFor={`Cookstep${step}`}>{`STEP${step}:`}</label>
+        <textarea
+          rows="5"
+          cols="30"
+          id={`Cookstep${step}`}
+          name={`Cookstep${step}`}
+          value={cookstepValue}
+          onChange={handleCookstepChange}
+          placeholder="예)요리방법 돼지고기 소고기 요리해줘"
+        />
+      </div>
     </>
   );
 }
