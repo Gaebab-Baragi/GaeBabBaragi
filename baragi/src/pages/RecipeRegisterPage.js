a38378a0b0549a7db8b2b2a81b0cb17f5e920eef
Hanul <hanulkim2000@naver.com>
2023-08-01 13:26:18 +0900

    .

--- baragi/src/pages/RecipeRegisterPage.js
+++ baragi/src/pages/RecipeRegisterPage.js
@@ -7,7 +7,6 @@ import CookStep from '../components/ui/CookStep';
 function RecipeRegisterPage() {
   return (
     <>
-    
     <div style={{ marginLeft:'10%' , marginRight : '10%' , marginTop : '0.5%' , marginBottom : '10%'}}>
         <h1 style={{ textAlign : 'left' }}>레시피 등록</h1>
         <InputImage></InputImage>
