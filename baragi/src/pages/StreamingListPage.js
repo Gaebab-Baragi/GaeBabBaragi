2a4f971cc651fc19b67a83df36d81eee495b3709
Hanul <hanulkim2000@naver.com>
2023-08-02 12:43:52 +0900

    FEAT) streaming axios 처리 중

--- baragi/src/pages/StreamingListPage.js
+++ baragi/src/pages/StreamingListPage.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { useState } from "react";
+import Streaming from "../streaming/Streaming";
+import { useSelector } from "react-redux";
+
 
 const BASE_URL = 'http://localhost:9999';
 
 function StreamingListPage() {
   const [streamingList, setStreamingList] = useState([]);
+  const user = useSelector((state) => (state.user));
 
   function getList() {
     axios.get(BASE_URL + '/meetings')
@@ -17,11 +21,19 @@ function StreamingListPage() {
       });
   }
 
-  function startMeeting(streamingItem) {
+  function joinMeeting(streamingItem) {
     console.log(streamingItem.id)
-    axios.post(BASE_URL + `/meetings/start/${streamingItem.id}`)
+    axios.get(BASE_URL + `/meetings/join-request/${streamingItem.id}`)
     .then((res)=>{
-      console.log('request success : ', res)
+      console.log('request success : ', res.data);
+
+      if (res.data.status === 'fail') {
+        alert(res.data.message);
+      } else {
+        // openvidu 세션 & 토근 요청 axios
+        console.log('openvidu axios request ')
+      }
+
     })
     .catch((err)=>{
       console.log('error occured' + err)
@@ -37,7 +49,8 @@ function StreamingListPage() {
             <p>ID: {streamingItem.id}</p>
             <p>Title: {streamingItem.title}</p>
             <p>Description: {streamingItem.description}</p>
-            <button onClick={()=>startMeeting(streamingItem)}>START STREAMING</button>
+            <button onClick={()=>joinMeeting(streamingItem)}>JOIN</button>
+            <Streaming sessionId={streamingItem.id} user={user}/>
             <hr/>
           </div>
         ))}
