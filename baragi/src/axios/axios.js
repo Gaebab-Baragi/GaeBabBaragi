4187494b74d3be089e408e96e3885c1ecda258fc
박영서 <pj0642@gmail.com>
2023-08-02 16:07:28 +0900

    fix) 배포 로그인 시도 중

--- baragi/src/axios/axios.js
+++ baragi/src/axios/axios.js
@@ -4,7 +4,7 @@ const current = "deploy";
 
 const URL = {
     local: "http://localhost:8083",
-    deploy : "https://doggy-yummy.site"
+    deploy : "https://doggy-yummy.site/"
 };
 
 const instance = axios.create({//백쪽으로 보내는 경우임
