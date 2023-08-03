ccaa8ee23ea23ca3ac6134af5228d40e5b8419cd
Park Yeongseo <pj0642@gmail.com>
2023-08-04 00:18:44 +0900

    fix)얌전히 다시 만들기

--- baragi/src/axios/axios.js
+++ baragi/src/axios/axios.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const current = "local";
 
 const URL = {
-    local: "http://localhost:8083/api",
-    deploy : "https://doggy-yummy.site/api"
+    local: "http://localhost:8083",
+    deploy : "https://doggy-yummy.site"
 };
 
 const instance = axios.create({//백쪽으로 보내는 경우임
-    baseURL: `${URL[current]}`,
+    baseURL: `${URL[current]}/api`,
     headers : {
         'Content-Type' : 'application/json'
     }
