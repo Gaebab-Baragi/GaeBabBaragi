904d0c6f6c894ffa0eaf5c3b3c937fb8b4fabe07
Hanul <hanulkim2000@naver.com>
2023-08-04 17:43:50 +0900

    FEAT) pet swift 하는중

--- baragi/src/pages/PetRegisterPage.js
+++ baragi/src/pages/PetRegisterPage.js
@@ -2,41 +2,55 @@ import './PetRegisterPage.css'
 import React, {useCallback, useEffect, useState,useRef} from "react";
 import PetRegisterForm from '../components/form/PetRegisterForm';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Navigation, Mousewheel } from 'swiper/modules';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function PetRegisterPage() {
   
 
   return(
-    <div className="pageContainer">
+    <div>
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
         slidesPerView={'auto'}
         coverflowEffect={{
-          rotate: 50,
+          rotate: 10,
           stretch: 0,
           depth: 100,
           modifier: 1,
           slideShadows: true,
         }}
         pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        navigation={true}
+        mousewheel={true}
+        modules={[EffectCoverflow, Pagination, ]}
         className="mySwiper"
       >
+
+      <div>
         <SwiperSlide>
           <PetRegisterForm/>
         </SwiperSlide>
         <SwiperSlide>
-          <PetRegisterForm/>
+          <div className='petForm'>
+            <PetRegisterForm/>
+          </div>
         </SwiperSlide>
+        <SwiperSlide>
+          <div className='petForm'>
+            <PetRegisterForm/>
+          </div>
+        </SwiperSlide>
+      </div>
       </Swiper>
-      
     </div>
+
   )
 }
 
