cf7fca3a8dac55d5eb12aef2b0012a8064760ec8
Hanul <hanulkim2000@naver.com>
2023-08-07 09:46:33 +0900

    REFACTOR) App.js import 수정

--- baragi/src/App.js
+++ baragi/src/App.js
@@ -20,8 +20,8 @@ import FindPasswordPage from './pages/FindPasswordPage';
 import RecipeDetailPage from './pages/RecipeDetailPage';
 import SocialLoginSuccessHandler from './components/social/SocialLoginSuccessHandler'
 import LogoutHandler from './components/social/LogoutHandler';
-import PetRegisterPage from './pages/PetRegisterPage';
-import StreamingLivePage from './streaming/StreamingLive';
+import PetRegisterPage from './pages/Pet/PetRegisterPage';
+import StreamingLivePage from './streaming/StreamingLivePage';
 import ObjectDetectionPage from './pages/ObjectDetectionPage';
 // -------------------PAGES-------------------//
 
