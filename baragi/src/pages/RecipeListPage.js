b825e84def89490258fa02bbfc799cd5308f28e9
Hanul <hanulkim2000@naver.com>
2023-08-10 17:07:35 +0900

    FEAT)filter 기능 완성

--- baragi/src/pages/RecipeListPage.js
+++ baragi/src/pages/RecipeListPage.js
@@ -16,14 +16,14 @@ function RecipeListPage() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   // 언제가져오는 거지? 자동 렌더링이 되나??
-  const requestHappen = useSelector((state) => state.recipeSearch.requestHappen);
   const dogs = useSelector((state)=>state.recipeSearch.dogs)
   const ingredients = useSelector((state)=>state.recipeSearch.ingredients)
   const title = useSelector((state)=>state.recipeSearch.keyword)
   const [filtered, setFiltered] = useState(false);
+  const [filteredList, setFilteredList] = useState([])
 
   useDidMountEffect(()=>{
-    console.log('리덕스에서 변화 일어남 감지')
+    console.log('리덕스 변화 페이지에서 감지')
     let tempIngredient = ingredients;
     if (ingredients.length === 0) {
       tempIngredient = null;
@@ -42,10 +42,13 @@ function RecipeListPage() {
     axios.post(process.env.REACT_APP_BASE_URL +'/api/recipes/searchlike', data)
       .then((res)=>{
         console.log('필터링 된 목록 가져오기 성공 : ' , res.data)
+        setFilteredList(res.data)
       })
       .catch((err)=>{
         console.log('필터링 데이터 가져오기 실패')
       })
+
+    setFiltered(true)
     },[dogs,ingredients])
 
   return (
@@ -62,7 +65,7 @@ function RecipeListPage() {
       // 검색 --> 레시피 목록 보이게
       ? 
       <div>
-        <h2>총 100 건의 레시피가 있습니다.</h2>
+        <h2>총 {filteredList.length} 건의 레시피가 있습니다.</h2>
         <CardPaginationList rowNum={3} />
       </div>
       // 첫 화면 --> 인기 레시피 캐로셀로 보여주기
