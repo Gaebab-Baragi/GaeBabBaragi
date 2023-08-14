5cc0fae76f453870720fd4a3a48f2ab9ee99f230
qocksdlf123 <qocksdlf123@naver.com>
2023-08-15 03:32:27 +0900

    ADD) initDb recipe추가

--- gaebap/src/main/java/site/doggyyummy/gaebap/domain/InitDb.java
+++ gaebap/src/main/java/site/doggyyummy/gaebap/domain/InitDb.java
@@ -89,6 +89,13 @@ public class InitDb {
             Ingredient ingredient34=createIngredient("베이킹파우더");
             Ingredient ingredient35=createIngredient("코코넛 가루");
             Ingredient ingredient36=createIngredient("식물성 오일");
+            Ingredient ingredient37=createIngredient("땅콩 버터");
+            Ingredient ingredient38=createIngredient("계피");
+            Ingredient ingredient39=createIngredient("레몬");
+            Ingredient ingredient40=createIngredient("파슬리");
+            Ingredient ingredient41=createIngredient("말린 켈프");
+            Ingredient ingredient42=createIngredient("말린 크렌베리");
+
 
 
             em.persist(ingredient1);
@@ -126,6 +133,12 @@ public class InitDb {
             em.persist(ingredient34);
             em.persist(ingredient35);
             em.persist(ingredient36);
+            em.persist(ingredient37);
+            em.persist(ingredient38);
+            em.persist(ingredient39);
+            em.persist(ingredient40);
+            em.persist(ingredient41);
+            em.persist(ingredient42);
 
 
 
@@ -384,9 +397,9 @@ public class InitDb {
             Step step94=createStep(recipe9,4L,"치즈를 넣고 골고루 섞어줍니다.");
             Step step95=createStep(recipe9,5L,"혼합물이 반죽이 되도록 손으로 치대줍니다.");
             Step step96=createStep(recipe9,6L,"반죽을 유산지 위에 올리고 밀대를 사용해서 반죽을 1cm 두께로 밀어주세요.");
-            Step step97=createStep(recipe9,6L,"쿠키틀로 반죽을 잘라주세요.");
-            Step step98=createStep(recipe9,6L,"베이킹 시트 위에 올리고 화씨 350도에서 20분 정도 구워줍니다.");
-            Step step99=createStep(recipe9,6L,"완전히 식힌 후 급여해주세요.");
+            Step step97=createStep(recipe9,7L,"쿠키틀로 반죽을 잘라주세요.");
+            Step step98=createStep(recipe9,8L,"베이킹 시트 위에 올리고 화씨 350도에서 20분 정도 구워줍니다.");
+            Step step99=createStep(recipe9,9L,"완전히 식힌 후 급여해주세요.");
 
             em.persist(step91);
             em.persist(step92);
@@ -421,9 +434,9 @@ public class InitDb {
             Step step104=createStep(recipe10,4L,"평평한 표면 위에 유산지를 깔고 반죽을 올려주세요.");
             Step step105=createStep(recipe10,5L,"밀대를 사용해서 반죽을 1cm 두께로 밀어주세요.");
             Step step106=createStep(recipe10,6L,"쿠키틀을 이용해 반죽을 잘라주세요.");
-            Step step107=createStep(recipe10,6L,"베이킹 시트 위에 올려주세요.");
-            Step step108=createStep(recipe10,6L,"화씨 370도에서 12-15분 정도 구워주세요.");
-            Step step109=createStep(recipe10,6L,"음식을 완전히 식힌 후 급여해주세요.");
+            Step step107=createStep(recipe10,7L,"베이킹 시트 위에 올려주세요.");
+            Step step108=createStep(recipe10,8L,"화씨 370도에서 12-15분 정도 구워주세요.");
+            Step step109=createStep(recipe10,9L,"음식을 완전히 식힌 후 급여해주세요.");
 
             em.persist(step101);
             em.persist(step102);
@@ -435,7 +448,303 @@ public class InitDb {
             em.persist(step108);
             em.persist(step109);
 
+            Recipe recipe11=createRecipe("닭가슴살 말랭이",member1,"우리 댕댕이들이 제일 좋아하는 닭가슴살 간식! 집에서 건조기 하나로 쉽게 만들어봐요!",
+                    url,LocalDateTime.now());
+            em.persist(recipe11);
+            RecipeIngredient recipeIngredient111=createRecipeIngredient(ingredient7,recipe11,"");
 
+            em.persist(recipeIngredient111);
+
+            Step step111=createStep(recipe11,1L,"닭고기를 먹기 좋은 크기로 얇게 썰어주세요.");
+            Step step112=createStep(recipe11,2L,"건조기를 준비해주세요.");
+            Step step113=createStep(recipe11,3L,"닭고기가 서로 닿지 않게 건조기 위에 나란히 올려주세요.");
+            Step step114=createStep(recipe11,4L,"뚜껑을 닫고 화씨 70도에서 8-10시간 동안 건조해주세요.");
+            Step step115=createStep(recipe11,5L,"완전히 식힌 후 급여해주세요.");
+
+            em.persist(step111);
+            em.persist(step112);
+            em.persist(step113);
+            em.persist(step114);
+            em.persist(step115);
+
+
+
+            Recipe recipe12=createRecipe("땅콩버터 사과 쿠키",member1,"비타민과 미네랄, 섬유질의 훌륭한 공급원인 사과! 쉽고 빠르게 만들 수 있는 땅콩버터 사과 쿠키 만들기!",
+                    url,LocalDateTime.now());
+            em.persist(recipe12);
+            RecipeIngredient recipeIngredient121=createRecipeIngredient(ingredient8,recipe12,"1개");
+            RecipeIngredient recipeIngredient122=createRecipeIngredient(ingredient1,recipe12,"100 g");
+            RecipeIngredient recipeIngredient123=createRecipeIngredient(ingredient36,recipe12,"2 tbsp");
+            RecipeIngredient recipeIngredient124=createRecipeIngredient(ingredient37,recipe12,"2 tbsp");
+            RecipeIngredient recipeIngredient125=createRecipeIngredient(ingredient28,recipe12,"1 cup");
+            RecipeIngredient recipeIngredient126=createRecipeIngredient(ingredient32,recipe12,"1 tbsp");
+
+            em.persist(recipeIngredient121);
+            em.persist(recipeIngredient122);
+            em.persist(recipeIngredient123);
+            em.persist(recipeIngredient124);
+            em.persist(recipeIngredient125);
+            em.persist(recipeIngredient126);
+
+            Step step121=createStep(recipe12,1L,"사과 줄기를 제거하고 껍질을 벗길 후 깍둑썰기 해주세요.");
+            Step step122=createStep(recipe12,2L,"그릇에 썰은 사과를 담고 통밀가루를 넣어주세요.");
+            Step step123=createStep(recipe12,3L,"가운데에 동그랗게 홈을 만들어 땅콩버터, 달걀, 꿀과 식물성 오일을 넣어주세요.");
+            Step step124=createStep(recipe12,4L,"다 넣었다면 잘 섞어 반죽으로 만들어주세요.");
+            Step step125=createStep(recipe12,5L,"평평한 표면 위에 유산지를 깔고 반죽을 올려 밀대를 사용해 반죽을 1cm 두께로 밀어주세요.");
+            Step step126=createStep(recipe12,6L,"쿠키틀로 반죽을 잘라주세요.");
+            Step step127=createStep(recipe12,7L,"자른 반죽을 베이킹 시트 위에 올려주세요.");
+            Step step128=createStep(recipe12,8L,"화씨 350도에서 12-15분 동안 구워주세요.");
+            Step step129=createStep(recipe12,9L,"음식을 완전히 식혀 급여해주세요.");
+
+            em.persist(step121);
+            em.persist(step122);
+            em.persist(step123);
+            em.persist(step124);
+            em.persist(step125);
+            em.persist(step126);
+            em.persist(step127);
+            em.persist(step128);
+            em.persist(step129);
+
+
+
+            Recipe recipe13=createRecipe("돼지고기 사과 간식",member1,"티아민, 니아신, 리보플라빈, 비타민 B-6, 인, 단백질, 아연, 칼륨 등 건강에 필요한 영양소의 훌륭한 공급원인 돼지고기와 강아지에게 변비, 소화 흡수, 입냄새 제거에 효과적인 사과로 만들어 보는 간식!",
+                    url,LocalDateTime.now());
+            em.persist(recipe13);
+            RecipeIngredient recipeIngredient131=createRecipeIngredient(ingredient1,recipe13,"");
+            RecipeIngredient recipeIngredient132=createRecipeIngredient(ingredient27,recipe13,"");
+            RecipeIngredient recipeIngredient133=createRecipeIngredient(ingredient38,recipe13,"");
+
+            em.persist(recipeIngredient131);
+            em.persist(recipeIngredient132);
+            em.persist(recipeIngredient133);
+
+            Step step131=createStep(recipe13,1L,"오븐용 그릇에 다진 돼지고기를 넣고 평평하게 눌러줍니다.");
+            Step step132=createStep(recipe13,2L,"사과 껍질과 씨를 제거하고 얇게 썰어 10분 동안 물이 가득 담긴 그릇에 담그세요.");
+            Step step133=createStep(recipe13,3L,"화씨 320도에서 25분 동안 구워주세요.");
+            Step step134=createStep(recipe13,4L,"사과 조각을 꽃 모양처럼 돌돌 말아주고 이쑤시개로 찔러 고정해주세요.");
+            Step step135=createStep(recipe13,5L,"구워진 돼지고기를 그릇에 옮겨 담고 사과 고명을 위에 올려주세요.");
+            Step step136=createStep(recipe13,6L,"계피 가루를 소량 뿌려주세요.");
+            Step step137=createStep(recipe13,7L,"사과에 꽂아진 이쑤시개를 빼고 급여해주세요.");
+
+            em.persist(step131);
+            em.persist(step132);
+            em.persist(step133);
+            em.persist(step134);
+            em.persist(step135);
+            em.persist(step136);
+            em.persist(step137);
+
+
+
+
+
+            Recipe recipe14=createRecipe("고구마 과자",member2,"비타민과 미네랄이 풍부한 댕댕이 최애간식 고구마! 고구마를 활용한 간단 고구마 과자 만들기!",
+                    url,LocalDateTime.now());
+            em.persist(recipe14);
+            RecipeIngredient recipeIngredient141=createRecipeIngredient(ingredient16,recipe14,"");
+            RecipeIngredient recipeIngredient142=createRecipeIngredient(ingredient18,recipe14,"");
+
+            em.persist(recipeIngredient141);
+            em.persist(recipeIngredient142);
+
+            Step step141=createStep(recipe14,1L,"고구마 끝 부분을 자르고 껍질을 벗겨 얇은 조각으로 썰어주세요.");
+            Step step142=createStep(recipe14,2L,"유산지가 깔린 베이킹 시트 위에 고구마 조각을 펼쳐주세요.");
+            Step step143=createStep(recipe14,3L,"고구마 조각 위에 오일을 발라주세요. 고구마를 뒤집어서 다른 한쪽 면에도 오일을 발라주세요.");
+            Step step144=createStep(recipe14,4L,"화씨 250도에서 1시간 30분 정도 구워주세요.");
+            Step step145=createStep(recipe14,5L,"완전히 식힌 후 급여해주세요.");
+
+            em.persist(step141);
+            em.persist(step142);
+            em.persist(step143);
+            em.persist(step144);
+            em.persist(step145);
+
+            Recipe recipe15=createRecipe("감자와 소고기",member1,"가장 먼저 감자는 빈혈을 예방해준다고 해요. 빈혈의 종류 중에서도 철결핍성 빈혈에 특히 도움이 되는데요. 감자에는 비타민C가 다량 함유되어 있고 이는 철과 결합하는 작용을 하기에 체내로의 흡수를 도와 빈혈을 방지해주는 효과를 볼 수 있답니다.",
+                    url,LocalDateTime.now());
+            em.persist(recipe15);
+            RecipeIngredient recipeIngredient151=createRecipeIngredient(ingredient39,recipe15,"1개");
+            RecipeIngredient recipeIngredient152=createRecipeIngredient(ingredient6,recipe15,"50g");
+            RecipeIngredient recipeIngredient153=createRecipeIngredient(ingredient13,recipe15,"100g");
+            RecipeIngredient recipeIngredient154=createRecipeIngredient(ingredient40,recipe15,"1tbsp");
+            RecipeIngredient recipeIngredient155=createRecipeIngredient(ingredient26,recipe15,"100g");
+
+            em.persist(recipeIngredient151);
+            em.persist(recipeIngredient152);
+            em.persist(recipeIngredient153);
+            em.persist(recipeIngredient154);
+            em.persist(recipeIngredient155);
+
+            Step step151=createStep(recipe15,1L,"레몬 껍질을 갈아넣은 후 모든 재료를 냄비에 넣고 10분간 조리한다.");
+            Step step152=createStep(recipe15,2L,"음식을 꺼내 접시에 담은 후 완전히 식으면 제공한다");
+
+            em.persist(step151);
+            em.persist(step152);
+
+
+
+            Recipe recipe16=createRecipe("사과 요거트 아이스크림 만들기",member1,"정말 뚝딱 맛도 좋고 건강에도 좋은 댕댕이 수제간식 만들기 ^^ 100점입니다아~",
+                    url,LocalDateTime.now());
+            em.persist(recipe16);
+            RecipeIngredient recipeIngredient161=createRecipeIngredient(ingredient1,recipe16,"100g");
+            RecipeIngredient recipeIngredient162=createRecipeIngredient(ingredient19,recipe16,"1cup");
+            RecipeIngredient recipeIngredient163=createRecipeIngredient(ingredient37,recipe16,"1tsp");
+            RecipeIngredient recipeIngredient164=createRecipeIngredient(ingredient32,recipe16,"1tsp");
+
+            em.persist(recipeIngredient161);
+            em.persist(recipeIngredient162);
+            em.persist(recipeIngredient163);
+            em.persist(recipeIngredient164);
+
+            Step step161=createStep(recipe16,1L,"사과와 피넛버터 꿀을 준비해주세요");
+            Step step162=createStep(recipe16,2L,"사과를 잘게 썰어 믹서기에 갈아 주세요");
+            Step step163=createStep(recipe16,3L,"믹서기에 간 사과에 꿀과 피넛버터 요거트를 넣고 섞어주세요");
+            Step step164=createStep(recipe16,4L,"아이스트레이에 넣고 4시간동안 얼리면 이제 끝>_<");
+
+            em.persist(step161);
+            em.persist(step162);
+            em.persist(step163);
+            em.persist(step164);
+
+
+            Recipe recipe17=createRecipe("오트밀 닭고기 정식",member1,"댕댕이가 제일 좋아하는 닭가슴살과 건강한 오트밀로 만들어 보는 건강 정식!",
+                    url,LocalDateTime.now());
+            em.persist(recipe17);
+            RecipeIngredient recipeIngredient171=createRecipeIngredient(ingredient8,recipe17,"1개");
+            RecipeIngredient recipeIngredient172=createRecipeIngredient(ingredient18,recipe17,"1 tbsp");
+            RecipeIngredient recipeIngredient173=createRecipeIngredient(ingredient30,recipe17,"1 cup");
+            RecipeIngredient recipeIngredient174=createRecipeIngredient(ingredient16,recipe17,"100g");
+            RecipeIngredient recipeIngredient175=createRecipeIngredient(ingredient41,recipe17,"1 tbsp");
+            RecipeIngredient recipeIngredient176=createRecipeIngredient(ingredient7,recipe17,"150g");
+
+            em.persist(recipeIngredient171);
+            em.persist(recipeIngredient172);
+            em.persist(recipeIngredient173);
+            em.persist(recipeIngredient174);
+            em.persist(recipeIngredient175);
+            em.persist(recipeIngredient176);
+
+            Step step1701=createStep(recipe17,1L,"고구마를 반으로 자르고 껍질을 벗겨주세요.");
+            Step step1702=createStep(recipe17,2L,"닭가슴살을 먹기 좋은 크기로 잘라주세요.");
+            Step step1703=createStep(recipe17,3L,"물 2컵이 담긴 냄비에 고구마와 달걀을 넣어 중불(화씨 350도)에서 10분 동안 삶아주세요.");
+            Step step1704=createStep(recipe17,4L,"냄비에서 달걀을 건져주세요.");
+            Step step1705=createStep(recipe17,5L,"달걀 껍질을 벗기고 몰드에 5분 동안 넣어주세요.");
+            Step step1706=createStep(recipe17,6L,"냄비에서 고구마를 건져 식혀주세요.");
+            Step step1707=createStep(recipe17,7L,"프라이팬에 올리브 오일을 두르고 센불(화씨 400도)에서 가열합니다.");
+            Step step1708=createStep(recipe17,8L,"닭가슴살을 넣고 5-7분 동안 재빨리 볶아주세요.");
+            Step step1709=createStep(recipe17,9L,"다 익은 닭가슴살을 접시에 담고 몰드에 있는 달걀을 꺼내 담아주세요.");
+            Step step1710=createStep(recipe17,10L,"프라이팬에 으깬 오트밀을 넣고 물 1/2컵을 부어주세요.");
+            Step step1711=createStep(recipe17,11L,"3-5분 동안 휘저으며 볶아주세요.");
+            Step step1712=createStep(recipe17,12L,"켈프를 넣고 1분 동안 볶아주세요.");
+            Step step1713=createStep(recipe17,13L,"몰드에 으깬 오트밀을 담아주세요.");
+            Step step1714=createStep(recipe17,14L,"큰 그릇에 고구마를 넣고 으깬 후 몰드에 넣어주세요.");
+            Step step1715=createStep(recipe17,15L,"강아지 그릇에 모든 재료들을 넣어주세요.");
+            Step step1716=createStep(recipe17,16L,"급여해주세요.");
+
+            em.persist(step1701);
+            em.persist(step1702);
+            em.persist(step1703);
+            em.persist(step1704);
+            em.persist(step1705);
+            em.persist(step1706);
+            em.persist(step1707);
+            em.persist(step1708);
+            em.persist(step1709);
+            em.persist(step1710);
+            em.persist(step1711);
+            em.persist(step1712);
+            em.persist(step1713);
+            em.persist(step1714);
+            em.persist(step1715);
+            em.persist(step1716);
+
+
+            Recipe recipe18=createRecipe("닭고기 비스킷",member1,"강아지들은 단백질이 풍부한 음식을 섭취해야 합니다. 닭고기처럼 맛있는 음식을 강아지에게 대접할 이유가 많습니다. 닭고기는 강아지 훈련용 간식으로 제공하셔도 좋으며 위가 예민한 강아지에게 밥과 함께 제공하셔도 좋습니다.",
+                    url,LocalDateTime.now());
+            em.persist(recipe18);
+            RecipeIngredient recipeIngredient181=createRecipeIngredient(ingredient28,recipe18,"1 cup");
+            RecipeIngredient recipeIngredient182=createRecipeIngredient(ingredient7,recipe18,"100g");
+            RecipeIngredient recipeIngredient183=createRecipeIngredient(ingredient8,recipe18,"1개");
+            RecipeIngredient recipeIngredient184=createRecipeIngredient(ingredient36,recipe18,"2tbsp");
+
+            em.persist(recipeIngredient181);
+            em.persist(recipeIngredient182);
+            em.persist(recipeIngredient183);
+            em.persist(recipeIngredient184);
+
+            Step step1801=createStep(recipe18,1L,"물 2-3컵이 채워진 냄비에 닭고기를 넣고 10-15분 정도 삶아주세요");
+            Step step1802=createStep(recipe18,2L,"닭고기를 꺼내주세요");
+            Step step1803=createStep(recipe18,3L,"삶은 물 1/2컵을 따로 보관한 뒤 식혀주세요");
+            Step step1804=createStep(recipe18,4L,"닭고기를 작은 조각으로 자르고 뼈를 제거해주세요");
+            Step step1805=createStep(recipe18,5L,"커다란 그릇에 밀가루, 닭고기, 식물성 오일, 달걀을 넣고 섞어주세요");
+            Step step1806=createStep(recipe18,6L,"준비해 둔 육수를 넣고 잘 섞어주세요");
+            Step step1807=createStep(recipe18,7L,"깨끗한 유산지 위에 반죽을 올려놓으세요");
+            Step step1808=createStep(recipe18,8L,"반죽에 밀가루를 뿌려주고 1cm 두께로 밀어주세요");
+            Step step1809=createStep(recipe18,9L,"네모꼴로 잘라주세요");
+            Step step1810=createStep(recipe18,10L,"화씨 325도에서 50-55분 정도 구워주세요");
+            Step step1811=createStep(recipe18,11L,"네모꼴로 잘라준 다음 완전히 식혀주세요");
+
+            em.persist(step1801);
+            em.persist(step1802);
+            em.persist(step1803);
+            em.persist(step1804);
+            em.persist(step1805);
+            em.persist(step1806);
+            em.persist(step1807);
+            em.persist(step1808);
+            em.persist(step1809);
+            em.persist(step1810);
+            em.persist(step1811);
+
+
+
+
+            Recipe recipe19=createRecipe("닭가슴살 육포",member1,"건조기를 이용한 닭가슴살 육포 만들기^^",
+                    url,LocalDateTime.now());
+            em.persist(recipe19);
+            RecipeIngredient recipeIngredient191=createRecipeIngredient(ingredient7,recipe19,"100g");
+
+            em.persist(recipeIngredient191);
+
+            Step step191=createStep(recipe19,1L,"첫번째 닭을 씻어서 가볍게 두드린다음");
+            Step step192=createStep(recipe19,2L,"얇게 자르고 조각 낸 닭 가슴살위에 파슬리를 뿌리세요.");
+            Step step193=createStep(recipe19,3L,"닭가슴살 조각들을 건조기에 고르게 놓은 다음 9시간을 건조시키세요");
+
+            em.persist(step191);
+            em.persist(step192);
+            em.persist(step193);
+
+
+
+
+            Recipe recipe20=createRecipe("바나나 땅콩버터 아이스크림",member1,"다가오는 여름 땅콩버터와 바나나 등 건강하고 맛 좋은 재료를 만드는 시원한 아이스크림 간식!",
+                    url,LocalDateTime.now());
+            em.persist(recipe20);
+            RecipeIngredient recipeIngredient201=createRecipeIngredient(ingredient2,recipe20,"100g");
+            RecipeIngredient recipeIngredient202=createRecipeIngredient(ingredient32,recipe20,"1tbsp");
+            RecipeIngredient recipeIngredient203=createRecipeIngredient(ingredient37,recipe20,"1/4cup");
+            RecipeIngredient recipeIngredient204=createRecipeIngredient(ingredient42,recipe20,"2tbsp");
+            RecipeIngredient recipeIngredient205=createRecipeIngredient(ingredient19,recipe20,"1cup");
+
+            em.persist(recipeIngredient201);
+            em.persist(recipeIngredient202);
+            em.persist(recipeIngredient203);
+            em.persist(recipeIngredient204);
+            em.persist(recipeIngredient205);
+
+            Step step201=createStep(recipe20,1L,"바나나 껍질을 벗기고 믹서기에 넣어주세요.");
+            Step step202=createStep(recipe20,2L,"꿀과 땅콩버터, 요거트를 넣어주세요.");
+            Step step203=createStep(recipe20,3L,"뚜껑을 닫고 갈아주세요.");
+            Step step204=createStep(recipe20,4L,"아이스크림 틀에 말린 크렌베리를 넣은 후 갈은 혼합물을 붓고 뚜껑을 닫아주세요.");
+            Step step205=createStep(recipe20,5L,"냉동실에 틀을 넣고 4시간 정도 얼려줍니다.");
+            Step step206=createStep(recipe20,6L,"급여해주세요.");
+
+            em.persist(step201);
+            em.persist(step202);
+            em.persist(step203);
+            em.persist(step204);
+            em.persist(step205);
+            em.persist(step206);
 
 
 //            Recipe recipe2=createRecipe("멍김밥2",member2,"맛있는 멍김밥을 만들어보아요. 우리 지산이도 좋아하는 음식을 여러분과 함께 만들고 싶어 레시피 공유합니다. 모두들 맛있게 만들고, 즐겨봅시다.",
