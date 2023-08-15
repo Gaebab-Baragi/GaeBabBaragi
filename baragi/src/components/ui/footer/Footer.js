876f2d30a288bbe4412d63b9f20d4fe45e715639
SeungAh-Yoo99 <ysa8497@gmail.com>
2023-08-15 22:06:12 +0900

    Design) footer 디자인 수정

--- baragi/src/components/ui/footer/Footer.js
+++ baragi/src/components/ui/footer/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
           <p>개밥바라기</p>
         </a>
         <div className="row gy-5">
-          <div className="col-lg-5 col-md-12 footer-info">
+          <div className="col-lg-4 col-md-12 footer-info">
             <div className="team-info">
               <h4>Contact Us</h4>
               <div className="team">
@@ -19,7 +19,7 @@ function Footer() {
               </div>
               <div className="team">
                 <span className="team-name">팀원 김선형: </span>
-                <span className="team-email"><ion-icon name="mail-open-outline"></ion-icon>tjsguddl96@gmail.com</span>
+                <span className="team-email"><ion-icon name="mail-open-outline"></ion-icon>tjsguddl96@gmail.com</span>&nbsp;
                 <span className="team-github"></span>
               </div>
               <div className="team">
@@ -39,23 +39,30 @@ function Footer() {
               </div>
               <div className="team">
                 <span className="team-name">팀원 유승아: </span>
-                <span className="team-email"><ion-icon name="mail-open-outline"></ion-icon>ysa8497@gmail.com</span>
+                <span className="team-email"><ion-icon name="mail-open-outline"></ion-icon>ysa8497@gmail.com</span>&nbsp;
                 <span className="team-github"><a href="https://github.com/SeungAh-Yoo99"><ion-icon name="logo-github"></ion-icon></a></span>
               </div>
             </div>
           </div>
 
-          <div className="col-lg-4 col-md-12 footer-our-project text-md-start">
-            <h4>Our Project</h4>
-            <p>
-              SSAFY 9기 서울 A703 <br/>
-              공통 프로젝트 (웹 기술)<br/>
-            </p>
-            <a href="https://lab.ssafy.com/s09-webmobile1-sub2/S09P12A703"><ion-icon name="logo-gitlab"></ion-icon></a>
+          <div className="col-lg-3 col-md-12 footer-watch-videos">
+            <h4>Watch Videos</h4>
+            <a className="video" href="">
+              <span className="video-icon"><ion-icon name="caret-forward-circle-outline"></ion-icon></span>
+                <span className="video-name">홍보 영상 보기</span>
+            </a>
+            <a className="video" href="">
+              <span className="video-icon"><ion-icon name="caret-forward-circle-outline"></ion-icon></span>
+              <span className="video-name">시연 영상 보기</span>
+            </a>
           </div>
 
-          <div className="col"></div>
-
+          <div className="col-lg-5 col-md-12 footer-our-project">
+            <h4>Our Project</h4>
+            <div className="project-info"><span>SSAFY 9기 서울 A703</span></div>
+            <div className="project-info"><span>공통 프로젝트 (웹 기술)</span></div>
+            <div className="project-info"><a href="https://lab.ssafy.com/s09-webmobile1-sub2/S09P12A703"><ion-icon name="logo-gitlab"></ion-icon></a></div>
+          </div>
         </div>
       </div>
 
